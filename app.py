35ac643603f2c53556030b8674a85342916bb212
Ashish Salaskar <ashishsalaskar1@gmail.com>
2019-04-01 00:55:18 +0530

    Added video Recognition

--- app.py
+++ app.py
@@ -23,26 +23,82 @@ def display_lines(image,lines):
     line_image = np.zeros_like(image)
     if lines is not None:
         for line in lines:
-            print(line)
+            #print(line)
             #convert from [[x1,y1,x2,y2]] -> [x1,y1,x2,y2]
             x1,y1,x2,y2 = line.reshape(4)
             #cv2.line(image,p1,p2,color,line width)
-            cv2.line(line_image, (x1,y1), (x2,y2), (255,0,0), 10)
+            cv2.line(line_image, (x1,y1), (x2,y2), (0,255,0), 10)
     return line_image
 
-image = cv2.imread('test_image.jpeg')
-lane_image = np.copy(image)
+def make_cordinates(image,line_param):
+    slope ,intercept = line_param
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1-intercept)/slope)
+    x2 = int((y2-intercept)/slope)
+    return np.array([x1,y1,x2,y2])
 
-canny = canny(lane_image)
-cropped_img = region_interest(canny)
+def average_slope_intercept(image,lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1,y1,x2,y2 = line.reshape(4)
+        parameters = np.polyfit((x1,x2), (y1,y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope,intercept))
+        else:
+            right_fit.append((slope,intercept))
+    
+    left_fit_avg = np.average(left_fit,axis=0)
+    right_fit_avg = np.average(right_fit,axis=0)
+    
+    left_line = make_cordinates(image,left_fit_avg)
+    right_line = make_cordinates(image,right_fit_avg)
+    
+    return np.array([left_line,right_line])
 
-#hough lines (image,grid size,precision,no of votes for cell,empty array,min Line,max Gap)
-lines = cv2.HoughLinesP(cropped_img, 2, np.pi/180, 100, np.array([]), minLineLength = 40,maxLineGap = 5)
+def mainDemo():
+    image = cv2.imread('test_image.jpeg')
+    lane_image = np.copy(image)
+    
+    canny_image = canny(lane_image)
+    cropped_img = region_interest(canny_image)
+    
+    #hough lines (image,grid size,precision,no of votes for cell,empty array,min Line,max Gap)
+    lines = cv2.HoughLinesP(cropped_img, 2, np.pi/180, 100, np.array([]), minLineLength = 40,maxLineGap = 5)
+    
+    averaged_line = average_slope_intercept(lane_image,lines)
+    #draw the found lines on original image
+    line_image = display_lines(lane_image,averaged_line)
+    #addWeighted(img1*0.8,img2*1,scalar)
+    combo_image = cv2.addWeighted(lane_image,0.8,line_image,1,1)
+    
+    cv2.imshow('Result',combo_image)
+    cv2.waitKey(0)
 
-#draw the found lines on original image
-line_image = display_lines(lane_image,lines)
-#addWeighted(img1*0.8,img2*1,scalar)
-combo_image = cv2.addWeighted(lane_image,0.8,line_image,1,1)
+cap = cv2.VideoCapture("test.mp4")
+while(cap.isOpened()):
+    _, frame = cap.read()
+    canny_image = canny(frame)
+    cropped_img = region_interest(canny_image)
+    
+    #hough lines (image,grid size,precision,no of votes for cell,empty array,min Line,max Gap)
+    lines = cv2.HoughLinesP(cropped_img, 2, np.pi/180, 100, np.array([]), minLineLength = 40,maxLineGap = 5)
+    
+    averaged_line = average_slope_intercept(frame,lines)
+    #draw the found lines on original image
+    line_image = display_lines(frame,averaged_line)
+    #addWeighted(img1*0.8,img2*1,scalar)
+    combo_image = cv2.addWeighted(frame,0.8,line_image,1,1)
+    cv2.imshow('Result',combo_image)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
 
-cv2.imshow('Result',combo_image)
-cv2.waitKey(0)
+
+
